8fb7ce69000109f22f995e13b6e9c3bce1f42ff9
Wazack <64Anex@gmail.com>
2022-10-15 23:51:59 +0200

    add: location

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -6,19 +6,25 @@ import DisplayWeather from './DisplayWeather.tsx/DisplayWeather';
 function App() {
 	const [isLoaded, setIsLoaded] = useState(false);
 
+    const [dataa , setdata] = useState({
+        info : "",
+        city : "Getting Data...",
+        temperature : "Getting Data...",
+        weather : "Getting Data...",
+        icon : "",
+        humidity : "Getting Data...",
+        wind_speed : "Getting Data...",
+        timestamp : "",
+    });
+
 	function success(pos: { coords: { latitude: any; longitude: any; }; }) {
-        // setpos ({
-        //     city : "",
-        //     latitude : pos.coords.latitude,
-        //     longitude : pos.coords.longitude
-        // });
     	setIsLoaded(true)
     }
 
     navigator.geolocation.getCurrentPosition(success);
 
 	return(
-		isLoaded?(<DisplayWeather/>) : <SearchCity/>
+		isLoaded?(<DisplayWeather dataa={dataa} setdata={setdata} />) : <SearchCity setIsLoaded={setIsLoaded} dataa={dataa} setdata={setdata} />
 	)
 }
 
---
+++ weather-app/src/Components/SearchBar/SearchBar.tsx
@@ -0,0 +1,43 @@
+import React, { MouseEventHandler, useRef } from 'react';
+import './SearchBar.scss'
+
+function SearchBar(props: any) {
+	const inputRef = useRef<HTMLInputElement>(null);
+
+
+	function foundCity(e: any) {
+		e.preventDefault();
+		// console.log('inputRef: ' + inputRef.current?.value);
+		fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&&lang=fr&appid=e46e0fbafc5076273c51ffb174510df0&units=metric`)
+			.then ((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				// console.log('city: ' + data.city.name);
+			})
+            .catch(function(err) {
+                console.log(err);
+            })
+		props.setIsLoaded(true);
+		console.log('foundCity');
+	}
+
+	return (
+		<form>
+			<input ref={inputRef} type="text" placeholder="Search..." required />
+			<button onClick={foundCity}>
+				<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<g clip-path="url(#clip0_8_45)">
+					<path d="M29.5898 25.9395L23.748 20.0977C23.4844 19.834 23.127 19.6875 22.752 19.6875H21.7969C23.4141 17.6191 24.375 15.0176 24.375 12.1875C24.375 5.45508 18.9199 0 12.1875 0C5.45508 0 0 5.45508 0 12.1875C0 18.9199 5.45508 24.375 12.1875 24.375C15.0176 24.375 17.6191 23.4141 19.6875 21.7969V22.752C19.6875 23.127 19.834 23.4844 20.0977 23.748L25.9395 29.5898C26.4902 30.1406 27.3809 30.1406 27.9258 29.5898L29.584 27.9316C30.1348 27.3809 30.1348 26.4902 29.5898 25.9395ZM12.1875 19.6875C8.04492 19.6875 4.6875 16.3359 4.6875 12.1875C4.6875 8.04492 8.03906 4.6875 12.1875 4.6875C16.3301 4.6875 19.6875 8.03906 19.6875 12.1875C19.6875 16.3301 16.3359 19.6875 12.1875 19.6875Z" fill="black"/>
+					</g>
+					<defs>
+					<clipPath id="clip0_8_45">
+					<rect width="30" height="30" fill="white"/>
+					</clipPath>
+					</defs>
+				</svg>
+			</button>
+		</form>
+	)
+}
+
+export default SearchBar;
